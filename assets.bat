bd2e842ff9af3a53c4ac1dbaae884424127d2ea0
Pedro Custodio <magneticplum@live.co.uk>
2014-07-03 13:20:35 +0100

    added assets folder and script to copy

---
+++ assets.bat
@@ -0,0 +1,10 @@
+:: script to copy new assets into the build folders
+@echo on
+
+:: remove old assets
+rmdir /s /q "platformer/Debug/assets"
+rmdir /s /q "platformer/Release/assets"
+
+:: copy new assets
+xcopy "assets" "platformer/Debug" /s /e /v
+xcopy "assets" "platformer/Release" /s /e /v
